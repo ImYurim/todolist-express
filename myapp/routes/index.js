cf4e748d6cfe8626327c1737850a4ca2516a823e
임유림 <ymyu124@naver.com>
2020-08-18 15:53:04 +0900

    cookie

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -22,13 +22,38 @@ router.get('/loginform',function(req,res,next){
   res.render('join/loginform');
 });
 
+function authIsOwner(req,res){
+  var isOwner = false;
+  var cookies = {};
+  if(req.headers.cookie){
+    cookies = cookie.parse(req.headers.cookie);
+  }
+  if(cookies.myemail=== 'yurim@naver.com'){
+    isOwner = true;
+  }
+  return isOwner;
+}
+
+
 router.post('/login',function(req,res,next){
+  var cookies = {};
   if(req.body.myemail!==undefined && req.body.mypassword !==undefined){
     var email = req.body.myemail;
     res.cookie('myemail',email);
+    
+  //   //바로 밑의 코드의 경우 모든 쿠키들이 다나옴 
+  //   console.log(req.headers.cookie);
+  //   //모든 쿠키들 중에서 이름으로 부르고 싶은경우 cookie 모듈을 이용해서 parse 해줘야함
+  //   cookies = cookie.parse(req.headers.cookie);
+  //   res.send(cookies.myemail);
+
+
+
   }
-  console.log(req.headers.cookie);
-  res.send(req.headers.cookie);
+
+  var isOwner = authIsOwner(req,res);
+  console.log(isOwner);
+  res.end();
 });
 
 module.exports = router;
