abf4791b1dcd89f8f53425c956bc544a12c0f2a9
임유림 <ymyu124@naver.com>
2020-08-18 14:30:02 +0900

    cookie

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var cookie = require('cookie');
+const { request } = require('../app');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  console.log(req.headers.cookie);    
+  var cookies ={};
+  //cookie가 없는 경우를 대비해서 if문으로 예외처리
+  if (req.headers.cookie !== undefined){
+    cookies = cookie.parse(req.headers.cookie);
+
+  }
+  //
+  res.cookie('mycookie','choco').cookie('mycookie2','strawberry');
+  console.log(cookies.mycookie);
   res.render('index', { title: 'To do list' });
 });
 
 module.exports = router;
+ 
