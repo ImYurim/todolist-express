9beaf14a12de5f8c5841b81bfd4591ae80add68a
임유림 <ymyu124@naver.com>
2020-08-18 16:17:30 +0900

    session-login

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,24 +3,7 @@ var router = express.Router();
 var cookie = require('cookie');
 const { request } = require('../app');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  console.log(req.headers.cookie);    
-  var cookies ={};
-  //cookie가 없는 경우를 대비해서 if문으로 예외처리
-  if (req.headers.cookie !== undefined){
-    cookies = cookie.parse(req.headers.cookie);
 
-  }
-  //
-  res.cookie('mycookie','choco',{maxAge: 900000}).cookie('mycookie2','strawberry');
-  console.log(cookies.mycookie);
-  res.render('index', { title: 'To do list' });
-});
-
-router.get('/loginform',function(req,res,next){
-  res.render('join/loginform');
-});
 
 function authIsOwner(req,res){
   var isOwner = false;
@@ -33,6 +16,34 @@ function authIsOwner(req,res){
   }
   return isOwner;
 }
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  var cookies ={};
+  // //cookie가 없는 경우를 대비해서 if문으로 예외처리
+  // if (req.headers.cookie !== undefined){
+  //   cookies = cookie.parse(req.headers.cookie);
+
+  // }
+  // //
+  // res.cookie('mycookie','choco',{maxAge: 900000}).cookie('mycookie2','strawberry');
+  // console.log(cookies.mycookie);
+  
+  //로그인 로그아웃 a 태그 구현
+  var isOwner = authIsOwner(req,res);
+  var userStatus = '로그인하기';
+  if(isOwner){
+    userStatus = '로그아웃하기';
+  }
+
+  res.render('index', { title: 'To do list', userStatus:userStatus });
+});
+
+router.get('/loginform',function(req,res,next){
+  res.render('join/loginform');
+});
+
 
 
 router.post('/login',function(req,res,next){
